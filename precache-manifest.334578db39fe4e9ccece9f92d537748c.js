cbefab97d159b52b84df41dc08052e3aacb99b06
RedstoneZockt <45714458+RedstoneZockt@users.noreply.github.com>
2021-06-24 16:23:30 +0200

    Updates

---
+++ precache-manifest.334578db39fe4e9ccece9f92d537748c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c75a0b5951c0a2f458ac97e000c2b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "876fbbe9a2ba68838c5c",
+    "url": "./static/css/main.9acbd221.chunk.css"
+  },
+  {
+    "revision": "40de93a603eb86c74d95",
+    "url": "./static/js/2.28565fa1.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "./static/js/2.28565fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "876fbbe9a2ba68838c5c",
+    "url": "./static/js/main.dcbd5763.chunk.js"
+  },
+  {
+    "revision": "dd7b323c43b450bc42d7",
+    "url": "./static/js/runtime-main.237a64a8.js"
+  }
+]);
